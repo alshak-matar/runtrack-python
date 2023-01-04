54cd1330da0e6367f87f238c45b46da6ab09a17a
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-01-04 22:44:11 +0100

    fonction qui prend 2 paramètres : - Le premier reçoit un String nommé “type” - Le second reçoit un String nommé “saison”

---
+++ jour02/job08.py
@@ -0,0 +1,14 @@
+def type_saison(type,saison):
+
+
+
+    if type=="fruit" and saison=="hiver":
+        print("orange, mandarine et kiwi")
+    elif type=="fruit" and saison=="ete":
+        print("Poire, fraise, cassis") 
+    elif type=="legume" and saison=="hiver":
+        print("carotte, topinambour, endive") 
+    elif type=="legume" and saison=="ete":
+        print("artichaut, aubergine,navet")
+
+type_saison("fruit","ete")

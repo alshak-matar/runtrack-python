2333f089c23f79de2b73e169c13545209ec732d7
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-01-04 22:42:58 +0100

    fonction qui prend en paramètre une String nommée “langage”.

---
+++ jour02/job07.py
@@ -0,0 +1,14 @@
+def tu_est_un(langage):
+
+    if langage=="javascript":
+        print("tu es un developpeur web")
+    elif langage=="python":
+        print("tu es un developpeur IA")
+    elif langage=="java":
+        print("tu es un developpeur logiciel")
+    elif langage=="reactjs":
+        print("tu es un dev mobile")
+    else:
+        print("Un jour, je serai le meilleur dev")
+
+tu_est_un("python")

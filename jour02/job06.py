2189b5f1dd24d2f8ceea0da196f8b0b427ace285
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-01-04 22:41:51 +0100

    fonction qui prend en paramètre un nombre nommé “nombre”.

---
+++ jour02/job06.py
@@ -0,0 +1,10 @@
+def positif_ou_negatif(nombre):
+
+    if nombre<0:
+        print("negatif")
+    elif nombre>0:
+        print("positif")
+
+positif_ou_negatif(2)
+positif_ou_negatif(-5)
+positif_ou_negatif(6)

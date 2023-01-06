8efef95ae90674992c38e531cb7f144c4c982ec1
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-01-06 03:52:32 +0100

    programme qui calcule la somme de toutes les valeurs paires de la liste L = [8, 24, 27, 48, 2,16, 9, 7, 84, 91]

---
+++ jour04/job08.py
@@ -0,0 +1,8 @@
+def min_max():
+
+    L = [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91]
+
+    print("Le chiffre le plus petit est :", min(L))
+    print("Le chiffre le plus grand est :", max(L))
+
+min_max()

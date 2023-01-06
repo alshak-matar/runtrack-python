93dd29cf364d0fa2968adc063ed5c4887c4c1d67
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-01-06 12:44:35 +0100

    programme qui affiche les nombres premiers jusqu’à 1000.

--- jour03/job05.py
+++ jour03/job05.py
@@ -1,4 +1,7 @@
-i = 1
-while i <= 1000:
-    print(i)
-    i = i + 1 
+for n in range(1001):
+   if n > 1:
+       for i in range(2,n):
+           if (n % i) == 0:
+               break
+       else:
+           print(n)

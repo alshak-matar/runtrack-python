95ed3c73f8f30af82d916773fe27f6fd7cc030cc
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-01-06 03:57:14 +0100

    programme qui créer la liste d’entiers L = [7, 11, 42, 39, 2], votre programme

---
+++ jour04/job10.py
@@ -0,0 +1,15 @@
+def produit():
+    L = [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91]
+    x = []
+
+    for i in L:
+        if i>=25 and i<=90:
+            x.insert(1,i)
+
+    produit = 1
+
+    for i in x:
+        produit = produit * i
+    print(produit)
+
+produit()
